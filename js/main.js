2f6c9b9efc7344500d1d44ee4504181eaf8835a1
JeroniLorenzo <jeronilorenzosolano@gmail.com>
2022-12-14 20:01:22 +0100

    añadida logica del juego

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 const listaCeldas = document.querySelectorAll('.tablero .celda')
-let turno=true
-const ganador= (i, j, k)=>{
-    movimientosArray[i] == movimientosArray[j] &&
-            movimientosArray[j] == movimientosArray[k]&&
-            movimientosArray[i] != null
-            return true
-}   
-const movimientosArray = new Array(9).fill(null)
-listaCeldas.forEach((celda, index)=>{
+let turno = true
+const movArray = new Array(9).fill(null)
+const ingresar = () => {
+    let jugador1 = document.getElementById('jugador1').value
+    let jugador2 = document.getElementById('jugador2').value
+    sessionStorage.setItem("jugador1", jugador1)
+    sessionStorage.setItem("jugador2", jugador2)
+}
+const ganar = (i, j, k) => {
+    if (movArray[i] == movArray[j] &&
+        movArray[j] == movArray[k] &&
+        movArray[i] != null) {
+        return true
+    } else false
+}
+const combGanadora = () => {
+    if (ganar(0, 1, 2)) {
+        return true
+    }
+    if (ganar(3, 4, 5)) {
+        return true
+    }
+    if (ganar(6, 7, 8)) {
+        return true
+    }
+    if (ganar(0, 3, 6)) {
+        return true
+    }
+    if (ganar(1, 4, 7)) {
+        return true
+    }
+    if (ganar(2, 5, 8)) {
+        return true
+    }
+    if (ganar(0, 4, 8)) {
+        return true
+    }
+    if (ganar(6, 4, 2)) {
+        return true
+    }
+    return false
+}
 
-    celda.addEventListener('click',(()=>{
-        if(turno){
+listaCeldas.forEach((celda, index) => {
+    celda.addEventListener('click', () => {
+        if (turno) {
             celda.classList.add('piezaX')
-        }else{
+        } else {
             celda.classList.add('piezaO')
         }
-        movimientosArray[index] = turno
+        movArray[index] = turno
+        const posGanador = combGanadora()
+        if (posGanador) {
+            window.location = "../pages/ganador.html"
+        }
         turno = !turno
-         if(ganador(0,1,2)){
-            /*Pasar a página de ganador*/
-            }
-    }),{once: true}  )
+    }, { once: true })
 })
