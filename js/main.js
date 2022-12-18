ee72720dc5e45d4525c3955503b33c8fdbb745f5
JeroniLorenzo <jeronilorenzosolano@gmail.com>
2022-12-18 20:46:50 +0100

    arreglado problema jugador2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const listaCeldas = document.querySelectorAll('.tablero .celda')
 let turno = true
-const movArray = new Array(8).fill("")
+const movArray = new Array(8).fill(null)
 listaCeldas.forEach((celda, posicion) => {
     celda.addEventListener('click', () => {
        if (turno) {
@@ -19,10 +19,10 @@ listaCeldas.forEach((celda, posicion) => {
 const ganar = (i, j, k) => {
     if (movArray[i] == movArray[j] &&
         movArray[j] == movArray[k] &&
-        movArray[i] != "") {
+        movArray[i] != null) {
         return true
         }
-     let empate = !movArray.includes('')
+     let empate = !movArray.includes(null)
           if(empate){
             window.location = "../pages/empate.html"
           }
@@ -52,13 +52,15 @@ const combGanadora = () => {
     if (ganar(6, 4, 2)) {
         return true
     }
-    return false
+    return null
 }
 
 const ingresar = ()=>{
     let jugador1 = document.querySelector('#jugador1').value
     let jugador2 = document.querySelector('#jugador2').value
     //   if(jugador1.length>0 && jugador2.lenght>0){
+/* Setting the value of the sessionStorage item called 'jugador1' to the value of the variable
+jugador1. */
         sessionStorage.setItem("jugador1", jugador1)
         sessionStorage.setItem("jugador2", jugador2)
         window.location = "../pages/tablero.html"
@@ -74,6 +76,8 @@ const mostrarNombres=()=>{
     const contNombre1 = document.querySelector('#contNombre1')
     const contNombre2 = document.querySelector('#contNombre2')
     const contTurno = document.querySelector('#contTurno')
+    /* Getting the value of the sessionStorage item called 'jugador1' and assigning it to the variable
+    nombre1. */
     let nombre1 = sessionStorage.getItem('jugador1')
     let nombre2 = sessionStorage.getItem('jugador2')
     let infoTurno = `Empieza ${nombre1} y sigue ${nombre2}`
